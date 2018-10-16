384516ca255d960b3d451708f5fa9014f8352dc5
berttheturt <30534071+berttheturt@users.noreply.github.com>
2018-10-16 12:36:40 -0500

    Update betcha_site/betcha_app/models.py
    Game model added

--- betcha_site/betcha_app/models.py
+++ betcha_site/betcha_app/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+import datetime
+from django.utils import timezone
 
-# Create your models here.
+class Game(models.Model):
+    team_A = models.CharField(max_length=200)
+    team_B = models.CharField(max_length=200)
+    line= DecimalField(5,1)
+    TV= models.DateTimeField()
+    pub_date = models.DateTimeField('date published')
+    
+    team_A_is_Favorite=models.BooleanField(default=False)
+    team_A_is_Home=models.BooleanField(default=False)
+    
+    team_A_score=models.IntegerField(default=0)
+	team_B_score=models.IntegerField(default=0)
+    
+    def __str__(self):
+        self_string=favoriteTeam+" vs "+underDog
+        return self.self_string
+
