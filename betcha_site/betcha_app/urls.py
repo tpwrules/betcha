c878bec127079e3cb07e3eb2e6e42461949d9ac2
Thomas Watson <twatson52@icloud.com>
2018-10-23 17:45:06 -0500

    wire in django login system

--- betcha_site/betcha_app/urls.py
+++ betcha_site/betcha_app/urls.py
@@ -1,7 +1,17 @@
-from django.urls import path
+from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+
+    path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
+    path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
+    path('accounts/password_change/',
+        auth_views.PasswordChangeView.as_view(),
+        name="password_change"),
+    path('accounts/password_change/done/',
+        auth_views.PasswordChangeDoneView.as_view(),
+        name="password_change_done"),
 ]
--- betcha_site/betcha_app/views.py
+++ betcha_site/betcha_app/views.py
@@ -1,11 +1,12 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
 from . import models
 
-
+@login_required
 def index(request):
     # for now, just load any games the database has
     games = models.Game.objects.all()
     return render(request, 'betcha_app/betting_website_sample.html', 
-        {"games": games})
+        {"games": games, "user": request.user})
