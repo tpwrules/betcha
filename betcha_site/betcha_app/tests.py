f638b4f96221c488422c301842d819aae9b9fc00
berttheturt <30534071+berttheturt@users.noreply.github.com>
2018-12-03 12:15:30 -0600

    Added a few test cases

--- betcha_site/betcha_app/tests.py
+++ betcha_site/betcha_app/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+import datetime
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Game
+
+def create_Game(teamA,teamB,teamAFav,teamAHome):
+    return Game.objects.create(team_A=teamA, team_B=teamB,team_A_is_favorite=teamAFav,team_A_is_home=teamAHome)
+    
+    
+class GameModelTests(TestCase):
+    def favorite_Test(self):
+        example=create_Game(teamA="dog",teamB="notDog",teamAFav=True,teamAHome=True)
+        self.assertIs(example.favorite(),"dog")
+ 
+        
