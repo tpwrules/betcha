723f3dd5f22c6cab2173a359a8ed87d602e94989
Thomas Watson <twatson52@icloud.com>
2018-12-03 18:58:27 -0600

    remove profile stuff since we never expanded on it

--- betcha_site/betcha_app/urls.py
+++ betcha_site/betcha_app/urls.py
@@ -7,8 +7,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('sheet/<int:week>/', views.sheet, name="sheet"),
 
-    path('profile/', views.profile, name="profile"),
-
     path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('accounts/password_change/',
