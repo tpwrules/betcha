6307d2e360988c2caa3f4019bb045490d44816e9
Thomas Watson <twatson52@icloud.com>
2018-12-04 11:25:35 -0600

    allow user to more easily change their password

--- betcha_site/betcha_app/urls.py
+++ betcha_site/betcha_app/urls.py
@@ -14,9 +14,6 @@ urlpatterns = [
     path('accounts/login/', auth_views.LoginView.as_view(), name="login"),
     path('accounts/logout/', auth_views.LogoutView.as_view(), name="logout"),
     path('accounts/password_change/',
-        auth_views.PasswordChangeView.as_view(),
-        name="password_change"),
-    path('accounts/password_change/done/',
-        auth_views.PasswordChangeDoneView.as_view(),
-        name="password_change_done"),
+        auth_views.PasswordChangeView.as_view(success_url="/"),
+        name="password_change")
 ]
